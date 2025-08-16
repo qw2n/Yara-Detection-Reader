8e9e15e929d91af9965a31627ff0ae6ef7532f59
Qwar1e <qwarlett@gmail.com>
2025-08-16 15:56:57 +1100

    Process.h

--- Yara Detection Reader/Yara Detection Reader/Source/Utils/Process/Process.h
+++ Yara Detection Reader/Yara Detection Reader/Source/Utils/Process/Process.h
@@ -36,10 +36,10 @@ namespace Console {
 namespace Types {
     struct YaraResult {
         int ReaderCount;
-        int SCAN_NEUTRAL;
         int SCAN_BAD_CERT;
-        int SCAN_MALICIOUS;
+        int SCAN_NEUTRAL;
         int SCAN_SUSPICIOUS;
         int SCAN_LIKELY_MALICIOUS;
+        int SCAN_MALICIOUS;
     };
 }
